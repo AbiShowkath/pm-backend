f040acb8e539a70d578fd8b428aa39614d77ea7d
AbiShowkath <abishowkath@ideas2it.com>
2025-08-06 13:50:58 +0530

    Updated teamController

--- src/controllers/teamController.ts
+++ src/controllers/teamController.ts
@@ -1,6 +1,6 @@
-import ProjectMembers from '../models/ProjectMembers.js';
 import Project from '../models/project.js';
 import User from '../models/user.js';
+import ProjectMembers from '../models/ProjectMembers.js';
 
 export async function getAllTeams(req, res, next) {
     try {
